01fb1986a93497e0c7390d07eddcd97c5c00ee24
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-02-13 16:40:25 +0200

    Add 400px condition to function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var size = 150
 
 function onBallClick(ball) {
+    if(ball.style.width === '400px') size = 100
     ball.style.width = size + 'px'
     ball.style.height = size + 'px'
     ball.innerText = ball.style.width
