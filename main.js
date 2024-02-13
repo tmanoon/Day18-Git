4518985d48e7992aac82867b3af9412c21d33f59
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-02-13 19:15:14 +0200

    Add parameter to function for balls

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 var gBallSize = 100
 
-function onBallClick(ball) {
+function onBallClick(ball, maxDiameter) {
     const randSize = getRandomIntInclusive(20, 60)
     ball.style.backgroundColor = getRandomColor()
     
-    if (ball.style.width >= '400px') {
+    if (gBallSize >= maxDiameter) {
         ball.style.width = 100 + 'px'
         ball.style.height = 100 + 'px'
         gBallSize = 100
-        
         ball.innerText = gBallSize
         return
     }
